a444ccc968f9993f1d512d2479ba10e40e729bff
IzumiMasaharu <muyunze2009@outlook.com>
2025-03-09 11:59:26 +0800

    init

---
+++ eureka-service/src/main/java/org/example/eurekasevice/EurekaSeviceApplication.java
@@ -0,0 +1,15 @@
+package org.example.eurekasevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSeviceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaSeviceApplication.class, args);
+    }
+
+}
---
+++ product-sevice/src/test/java/org/example/productsevice/ProductSeviceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.productsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSeviceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
